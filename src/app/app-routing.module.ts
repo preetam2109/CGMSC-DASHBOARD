2dc663ba832333400bb14377543c011ffbafbc13
Lomeshwarsahu <lomeshwarsahu1996@gmail.com>
2025-10-09 11:06:50 +0530

    qc and abc  update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -142,6 +142,8 @@ import { TenderStatusOracleComponent } from './component/tender-status-oracle/te
 import { PoPlanningComponent } from './component/ORACLE-DASHBOARD/po-planning/po-planning.component';
 import{NearExpiryOracleComponent} from './component/near-expiry-oracle/near-expiry-oracle.component';
 import { PipelineSuppliesOracleComponent } from './component/pipeline-supplies-oracle/pipeline-supplies-oracle.component';
+import { ABCVEDSDEAnalysisOracleComponent } from './component/abc-ved-sde-analysis-oracle/abc-ved-sde-analysis-oracle.component';
+import { QCAnalysisOracleComponent } from './qc-analysis-oracle/qc-analysis-oracle.component';
 
 // import { ConversationHodCgmscComponent } from './component/tender-status /conversation-hod-cgmsc/conversation-hod-cgmsc.component';
 // import { TenderStatusDashComponent } from './component/tender-status /tender-status-dash/tender-status-dash.component';
@@ -286,6 +288,8 @@ const routes: Routes = [
 {path:'oracle-dashboard',component:TenderStatusOracleComponent,canActivate:[RouteGuardService],data: { allowedRoles: ['DHS','SEC1','CME','DME1','Warehouse','DM PO','QC','QC2','Tenders','Division']} },
 {path:'Near-Expiry-Oracle',component:NearExpiryOracleComponent,canActivate:[RouteGuardService],data: { allowedRoles: ['DHS','SEC1','CME','DME1','Warehouse','DM PO','QC','QC2','Tenders','Division']} },
 {path:'PipelineSuppliesOracle',component:PipelineSuppliesOracleComponent,canActivate:[RouteGuardService],data: { allowedRoles: ['DHS','SEC1','CME','DME1','Warehouse','DM PO','QC','QC2','Tenders','Division']} },
+{path:'ABCVEDSDEAnalysisOracle',component:ABCVEDSDEAnalysisOracleComponent,canActivate:[RouteGuardService],data: { allowedRoles: ['DHS','SEC1','CME','DME1','Warehouse','DM PO','QC','QC2','Tenders','Division']} },
+{path:'QCAnalysisOracle',component:QCAnalysisOracleComponent,canActivate:[RouteGuardService],data: { allowedRoles: ['DHS','SEC1','CME','DME1','Warehouse','DM PO','QC','QC2','Tenders','Division']} },
 {path:'powerbidash',component:PowerbidashComponent,canActivate:[RouteGuardService],data: { allowedRoles: ['DHS','SEC1','CME','DME1','Warehouse','DM PO','QC','QC2','Tenders','Division']} },
 {path:'tender-status-cme',component:TenderStatusDashCmeComponent,canActivate:[RouteGuardService],data: { allowedRoles: ['DHS','SEC1','CME','DME1','Warehouse','DM PO','QC','QC2','Tenders','Division']} },
 {path:'conversationHodCgmsc',component:ConversationHodCgmscComponent,canActivate:[RouteGuardService],data: { allowedRoles: ['DHS','SEC1','CME','DME1','Warehouse','DM PO','QC','QC2']} },
