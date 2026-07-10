b0f67cdc90b8a347a9aa78697f4a2b2f15fb8155
preetam2109 <preetamlahre2109@gmail.com>
2026-07-10 17:40:18 +0530

    Commit changes before pulling from lomesh

--- src/app/component/Infrastructure-Components/infrastructure-home/infrastructure-home.component.ts
+++ src/app/component/Infrastructure-Components/infrastructure-home/infrastructure-home.component.ts
@@ -566,7 +566,7 @@ export class InfrastructureHomeComponent implements OnInit {
   }
 
   DetailProgress(did: any, dashname: any, nosworks: any): void {
-    // debugger;
+    // ;
     this.dashname = dashname;
     this.nosworks = nosworks;
     this.spinner.show();
